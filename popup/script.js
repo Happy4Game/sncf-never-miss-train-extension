3abf4649475c8568a0cbabca011c868db49413b8
Happy4Game <dufeutrel.thibaut@gmail.com>
2025-12-15 20:52:08 +0100

    feat: search for station

--- popup/script.js
+++ popup/script.js
@@ -5,3 +5,34 @@ async function fetchDeparture(idStation) {
   const departureList = await request.json();
   return departureList;
 }
+
+async function fetchStationList(search = "%") {
+  const param = new URLSearchParams({ libelle: search });
+  const request = await fetch(
+    `https://www.sncf-voyageurs.com/api/bff/locations?${param}&typeEmplacement=ZONE_ARRET&component=schedule`
+  );
+  return await request.json();
+}
+
+window.onload = (event) => {
+  document.querySelector("#station").addEventListener("input", (ev) => {
+    const datalist = document.querySelector("datalist");
+    // Check if option is already in datalist
+    for (const o of datalist.options) {
+      if (o.value == ev.target.value) {
+        alert(o.dataset.code);
+      }
+    }
+    fetchStationList(ev.target.value).then((list) => {
+      while (datalist.firstChild) {
+        datalist.removeChild(datalist.firstChild);
+      }
+      for (const l of list) {
+        const option = document.createElement("option");
+        option.dataset.code = l.code;
+        option.value = l.libelle;
+        datalist.appendChild(option);
+      }
+    });
+  });
+};
