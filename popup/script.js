2311d980d202953fcf99bfbc211b3935a4df18d9
Happy4Game <dufeutrel.thibaut@gmail.com>
2026-02-09 16:48:54 +0100

    refactor: replace window.localStorage with localStorage for consistency

--- popup/script.js
+++ popup/script.js
@@ -27,38 +27,34 @@ window.onload = (event) => {
       // Check if option is already in datalist
       for (const o of datalist.options) {
         if (o.value == inputCity.value) {
-          window.localStorage.setItem("trainStationCityCode", o.dataset.code);
+          localStorage.setItem("trainStationCityCode", o.dataset.code);
         }
       }
 
-      if (window.localStorage.getItem("trainStationCityCode") !== "") {
+      if (localStorage.getItem("trainStationCityCode") !== "") {
         document.querySelector("#last-refresh-time").innerHTML = "pending";
         fetchDepartures(
-          window.localStorage.getItem("trainStationCityCode")
+          localStorage.getItem("trainStationCityCode")
         ).then((list) => {
-          window.localStorage.setItem(
+          localStorage.setItem(
             "lastRefresh",
             parseDateToHourMinSec(new Date())
           );
 
           document.querySelector("#last-refresh-time").innerHTML =
-            window.localStorage.getItem("lastRefresh");
+            localStorage.getItem("lastRefresh");
 
           const departureListDiv = document.querySelector(
             "#list-departures > tbody"
           );
           while (departureListDiv.firstChild) {
-            departureListDiv.removeChild(departureListDiv.firstChild);
+            departureListDiv.firstChild.remove();
           }
           for (const l of list) {
             const departureDiv = document.createElement("tr");
             const destination = l.traffic.destination;
-            const track = l.platform.track;
-            const detailsURL = l.TrafficDetailsUrl;
             const scheduledTime = new Date(l.scheduledTime);
-            const actualTime = new Date(l.actualTime);
             const informationStatus = l.informationStatus;
-            const presentation = l.presentation;
             const platform = l.platform;
             departureDiv.innerHTML = `
               <td>${destination}</td>
@@ -73,9 +69,9 @@ window.onload = (event) => {
               </td>
               <td>
                 ${
-                  informationStatus.delay !== null
-                    ? `${informationStatus.delay}min`
-                    : ""
+                  informationStatus.delay === null
+                    ? ""
+                    : `${informationStatus.delay}min`
                 }
               </td>
               <td class="set-tracking-btn" 
@@ -84,9 +80,9 @@ window.onload = (event) => {
                 data-status="${informationStatus?.trainStatus}"
                 data-track="${platform.isTrackactive ? platform.track : ""}"
                 data-delay="${
-                  informationStatus.delay !== null
-                    ? `${informationStatus.delay}min`
-                    : ""
+                  informationStatus.delay === null
+                    ? ""
+                    : `${informationStatus.delay}min`
                 }"
               style="background-color: #e5e5e5; cursor: pointer;">🚆
               </td>
@@ -101,11 +97,11 @@ window.onload = (event) => {
   // Set trainStationCity and set
   document.querySelector("#station").addEventListener("input", (ev) => {
     const datalist = document.querySelector("datalist");
-    window.localStorage.setItem("trainStationCity", ev.target.value);
-    window.localStorage.setItem("trainStationCityCode", "");
+    localStorage.setItem("trainStationCity", ev.target.value);
+    localStorage.setItem("trainStationCityCode", "");
     fetchStationList(ev.target.value).then((list) => {
       while (datalist.firstChild) {
-        datalist.removeChild(datalist.firstChild);
+        datalist.firstChild.remove()
       }
       for (const l of list) {
         const option = document.createElement("option");
