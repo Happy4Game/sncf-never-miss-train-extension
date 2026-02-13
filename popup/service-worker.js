9f4de01b12650f57ee033700af2e12512b4ef57d
Happy4Game <dufeutrel.thibaut@gmail.com>
2026-02-13 10:20:05 +0100

    refactor: simplifies changes of chrome badge

--- popup/service-worker.js
+++ popup/service-worker.js
@@ -1,9 +1,7 @@
 import {
+  setChromeBadge,
   fetchDepartures,
   parseDateToHourMin,
-  setChromeBadgeGreen,
-  setChromeBadgeOrangeAndLate,
-  setChromeBadgeRed,
 } from "./utils.js";
 
 let trainToTrack = null;
@@ -39,22 +37,7 @@ setInterval(
       );
       console.log("fetch by service worker", matchedDeparture);
 
-      if (matchedDeparture) {
-        const timeCropped = matchedDeparture.delay.substring(
-          0,
-          matchedDeparture.delay.length - 2,
-        );
-        if (matchedDeparture.status === "Ontime") {
-          setChromeBadgeGreen();
-        } else if (matchedDeparture.status === "RETARD") {
-          setChromeBadgeOrangeAndLate(timeCropped);
-        } else if (
-          matchedDeparture.status === "SUPPRESSION_TOTALE" ||
-          matchedDeparture.status === "SUPPRESSION_PARTIELLE"
-        ) {
-          setChromeBadgeRed();
-        }
-      }
+      setChromeBadge(matchedDeparture);
     }
   },
   15 * 1000 * 1,
